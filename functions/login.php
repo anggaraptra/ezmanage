b0edad5d0b2ceba0a064ceb54a41e7411e635c9c
anggaraptra <kadekanggaraptra@gmail.com>
2025-06-10 21:59:40 +0800

    update: update session login, user id, set cookie

--- functions/login.php
+++ functions/login.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'functions.php';
 
-if (isset($_POST)) {
+if (isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
@@ -9,22 +9,25 @@ if (isset($_POST)) {
     if (mysqli_num_rows($result) === 1) {
         $user = mysqli_fetch_assoc($result);
         if (password_verify($password, $user['password_hash'])) {
-            // set flash message
-            setFlash('auth', 'Login berhasil!', 'success');
-            // Set session variables
+            $_SESSION['login'] = true; // Set session variable to indicate user is logged in
             $_SESSION['user']['id'] = $user['id'];
-            $_SESSION['user']['username'] = $user['username'];
-            $_SESSION['user']['fullname'] = $user['fullname'];
-            $_SESSION['user']['email'] = $user['email'];
+            // set flash message
+            setFlash('auth', 'Login successful!', 'success');
+            // Set session variables
+            if (isset($_POST['remember'])) {
+                // Set cookie for remember me (set path and httponly for reliability)
+                setcookie('id', $user['id'], time() + 600, '/', '', false, true);
+                setcookie('key', hash("sha256", $user["username"]), time() + 600, '/', '', false, true);
+            }
             header("Location: ../index.php");
             exit();
         } else {
             // set flash message
-            setFlash('auth', 'Username atau Password salah!', 'error');
+            setFlash('auth', 'Username or Password incorrect!', 'error');
         }
     } else {
         // set flash message
-        setFlash('auth', 'Username atau Password salah!', 'error');
+        setFlash('auth', 'Username or Password incorrect!', 'error');
     }
 }
 header("Location: ../login_page.php");
