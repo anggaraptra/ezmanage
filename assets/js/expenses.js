b10eeb35346b379fd84e28dcebc31346f0e221d1
anggaraptra <kadekanggaraptra@gmail.com>
2025-06-10 16:02:04 +0800

    update: open modal edit, delete categories expenses

--- assets/js/expenses.js
+++ assets/js/expenses.js
@@ -8,3 +8,23 @@ btn.addEventListener('click', function (e) {
 document.addEventListener('click', function () {
   menu.classList.add('hidden');
 });
+
+function openEditCategoryModal(id, name) {
+  document.getElementById('edit_category_id').value = id;
+  document.getElementById('edit_category_name').value = name;
+  document.getElementById('editCategoryModal').classList.remove('hidden');
+}
+
+function closeEditCategoryModal() {
+  document.getElementById('editCategoryModal').classList.add('hidden');
+}
+
+function openDeleteCategoryModal(id, name) {
+  document.getElementById('delete_category_id').value = id;
+  document.getElementById('delete_category_name').textContent = name;
+  document.getElementById('deleteCategoryModal').classList.remove('hidden');
+}
+
+function closeDeleteCategoryModal() {
+  document.getElementById('deleteCategoryModal').classList.add('hidden');
+}
