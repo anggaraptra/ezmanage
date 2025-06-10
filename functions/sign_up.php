086dc6ee1f4c15cb0dfb1cf4779142865bac858b
anggaraptra <kadekanggaraptra@gmail.com>
2025-06-10 22:00:58 +0800

    update: check input and validation

--- functions/sign_up.php
+++ functions/sign_up.php
@@ -4,18 +4,46 @@ require_once 'functions.php';
 if (isset($_POST)) {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $konfirmasi = $_POST['konfirmasi'];
+    $konfirmasi = $_POST['confirm'];
     $fullname = $_POST['fullname'];
     $email = $_POST['email'];
     $city = $_POST['city'];
 
+    // Validasi input
+    if (empty($username) || empty($password) || empty($fullname) || empty($email) || empty($city)) {
+        setFlash('auth', 'Semua field harus diisi!', 'error');
+        header("Location: ../register.php");
+        exit;
+    }
+    // Cek apakah username sudah ada
+    $check = dbquery("SELECT id FROM users WHERE username = '$username'");
+    if ($check && mysqli_num_rows($check) > 0) {
+        setFlash('auth', 'Username sudah terdaftar!', 'error');
+        header("Location: ../register.php");
+        exit;
+    }
+    // Cek apakah password dan konfirmasi password sama
+    if ($password !== $konfirmasi) {
+        setFlash('auth', 'Password dan Konfirmasi Password tidak sama!', 'error');
+        header("Location: ../register.php");
+        exit;
+    }
+    // cek apakah email sudah terdaftar
+    $emailCheck = dbquery("SELECT id FROM users WHERE email = '$email'");
+    if ($emailCheck && mysqli_num_rows($emailCheck) > 0) {
+        setFlash('auth', 'Email sudah terdaftar!', 'error');
+        header("Location: ../register.php");
+        exit;
+    }
+
+    // Hash password
     $password = password_hash($password, PASSWORD_DEFAULT);
 
-    $result = dbquery("INSERT INTO users (username, fullname, email, city, password_hash) VALUES ('$username', '$fullname', '$email', '$city', '$password')");
+    $result = dbquery("INSERT INTO users (username, fullname, email, city, password_hash, profile_pic) VALUES ('$username', '$fullname', '$email', '$city', '$password', '')");
     if ($result) {
-        setFlash('register', 'Data member berhasil disimpan!', 'success'); // Set flash message untuk simpan
+        setFlash('auth', 'Berhasil Daftar Akun, Silahkan Login!', 'success');
     } else {
-        setFlash('register', 'Data member gagal disimpan!', 'error'); // Set flash message untuk simpan
+        setFlash('auth', 'Gagal Daftar Akun!', 'error');
     }
 }
 header("Location: ../register.php");
